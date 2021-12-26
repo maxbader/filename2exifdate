8e36a4998f6378b54cbd2dfbb22f3662fa389d3c
Markus Bader <markus.bader@mx-robotics.com>
2021-12-26 18:33:17 +0100

    first version

--- Readme.md
+++ Readme.md
@@ -1,11 +1,39 @@
 # file name to exif date
 This python script is able to parse file names of media date such as _jpg_ or _mp4_ files.
 It detects encoded time stamps and uses this time stamps to set exif information.
-## Installation
-using conda
+## Usage
 ```
-conda create -n pyexif python
-conda activate pyexif
-pip install exifread python-dateutil
+usage: filenamedate2exifdate.py [-h] --input i [i ...] [--prefix p] [--format f] [--exifcmd EXIFCMD] [--date_tags DATE_TAGS [DATE_TAGS ...]] [--echo] [--echo_only]
+
+options:
+  -h, --help            show this help message and exit
+  --input i [i ...]     input file --> required
+  --prefix p            file prefix
+  --format f            date format YYYY-MM-DD-hh-mm-ss-ccc
+  --exifcmd EXIFCMD     exiftool command
+  --date_tags DATE_TAGS [DATE_TAGS ...]
+                        exif tags to set such as alldates, FileModifyDate, CreateDate, ...
+  --echo                prints the exiftool command
+  --echo_only           used to prevent the command execution
 ```
+### examples
+#### single images
+Signal images
+```
+python ./filenamedate2exifdate.py --echo_only --prefix signal- --format YYYY-MM-DD-hh-mm-ss-ccc --date_tags alldates FileModifyDate --input signal-2021-02-20-08-59-20-104.jpg 
+```
+WhatsApp video
+```
+python ./filenamedate2exifdate.py --echo_only --prefix VID- --format YYYYMMDD-WAcccc --date_tags alldates FileModifyDate --input VID-20190609-WA0019.mp4 
+```
+WhatsApp images
+```
+python ./filenamedate2exifdate.py --echo_only --prefix IMG- --format YYYYMMDD-WAcccc --date_tags alldates FileModifyDate --input IMG-20190721-WA0002.jpg 
+```
+#### patch processing
+Signal images
+```
+find . -name 's*.jpg' -exec python ./filenamedate2exifdate.py --echo_only --prefix signal- --format YYYY-MM-DD-hh-mm-ss-ccc --date_tags alldates FileModifyDate --input {} \;
+```
+
 
--- filenamedate2exifdate.py
+++ filenamedate2exifdate.py
@@ -1,23 +1,73 @@
-import exifread
 import argparse
-import dateutil.parser as dparser
-parser = argparse.ArgumentParser()
-parser.add_argument('files', metavar='f', type=str, nargs='+',
-                    help='file names')
-args = parser.parse_args()
+import os
 
 
-# Open image file for reading (binary mode)
-for filename in args.files:
-    dparser.parse(filename)
-    f = open(filename, 'rb')
+def get_file_date_str(filename, format):
+    format_list = list(format)
+    filename_list = list(filename)
+    year = str()
+    month = str()
+    day = str()
+    hour = str()
+    min = str()
+    sec = str()
+    milli = str()
+    date_str = str()
+    for i in range(0, len(format_list)):
+        c = filename_list[i]
+        if c.isdigit():
+            if format_list[i] == 'Y': year = year + c
+            elif format_list[i] == 'M': month = month + c
+            elif format_list[i] == 'D': day = day + c
+            elif format_list[i] == 'h': hour = hour + c
+            elif format_list[i] == 'm': min = min + c
+            elif format_list[i] == 's': sec = sec + c
+            elif format_list[i] == 'c': milli = milli + c
+            else : return str()
+            
+    if len(year)  != 4: year = '1970'
+    if len(month) != 2: month = '01'
+    if len(day) != 2: day = '01'
+    if len(hour) != 2: hour = '00'
+    if len(min) != 2: min = '00'
+    if len(sec) != 2: sec = '00'
+    if len(milli) == 0: milli = '00'
 
-    # Return Exif tags
-    tags = exifread.process_file(f)
-    print("{0} ".format(tags))
-    for k, v in tags.items():
-        print("{0} {1}".format(k, v))
-    
-    for tag in tags.keys():
-        if tag not in ('JPEGThumbnail', 'TIFFThumbnail', 'Filename', 'EXIF MakerNote'):
-            print("Key: {0} , value {1}".format(tag, tags[tag]))
+    date_str = year + '-' + month + '-' + day + ' ' + hour + ':' + min + ':' + sec + '-' + milli
+    return date_str
+
+
+
+def set_exif_date(filename, date_str, date_tags, exifcmd, echo, echo_only):
+
+    command = exifcmd + " -overwrite_original "
+    for tag in date_tags:
+        command = command + "'-" + tag + "=" + date_str + "'"
+    command = command + " " + filename
+    if echo or echo_only:
+        print(command)
+    if not echo_only:
+        os.system(command)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input', metavar='i', type=str, nargs='+', required=True, help='input file')
+    parser.add_argument("--prefix", metavar='p', type=str, default='signal-', help="file prefix")
+    parser.add_argument("--format", metavar='f', type=str, default='YYYY-MM-DD-hh-mm-ss-ccc', help="date format YYYY-MM-DD-hh-mm-ss-ccc")
+    parser.add_argument("--exifcmd", type=str, default='exiftool', help="exiftool command")
+    parser.add_argument('--date_tags', type=str, nargs='+', default=['alldates'],  help='exif tags to set such as alldates, FileModifyDate, CreateDate, ...')
+    parser.add_argument('--echo', action='store_true', help="prints the exiftool command")
+    parser.add_argument('--echo_only', action='store_true', help="used to prevent the command execution")
+    args = parser.parse_args()
+
+    for filename in args.input:
+        basename = os.path.basename(filename)
+        date_str = get_file_date_str(basename.replace(args.prefix, ""), args.format)
+        if not date_str:
+            print("Error: check the date format: {}{} and the file name: {}".format(args.prefix, args.format, filename))
+            exit()
+        set_exif_date(filename, date_str, args.date_tags, args.exifcmd, args.echo, args.echo_only)
+
+
+if __name__ == "__main__":
+    main()
